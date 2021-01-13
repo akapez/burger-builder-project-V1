b4389e83d62dbdb79ea8d3e5829b88197df9dafd
avishka964 <avishkakapuruge@gmail.com>
2021-01-13 12:51:58 +0530

    implementing side drawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,17 @@
 import React from "react"
 import classes from "./Toolbar.css"
+import Logo from "../../Logo/Logo"
+import NavigationItems from "../NavigationItems/NavigationItems"
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
-    <nav>...</nav>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
 )
 
