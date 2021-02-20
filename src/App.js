6b8d54531df8f9a3998436675e279b8028372088
avishka964 <avishkakapuruge@gmail.com>
2021-02-20 14:08:09 +0530

    queryparam set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
-import { Route } from "react-router-dom"
+import { Route, Switch, withRouter, Redirect } from "react-router-dom"
+import { connect } from "react-redux"
 import Layout from "./components/Layout/Layout"
 
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
@@ -7,8 +8,13 @@ import Checkout from "./containers/Checkout/Checkout"
 import Orders from "./containers/Orders/Orders"
 import Auth from "./containers/Auth/Auth"
 import Logout from "./containers/Auth/Logout/Logout"
+import * as actions from "./store/actions/index"
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup()
+  }
+
   // state = {
   //   show: true,
   // }
@@ -20,18 +26,44 @@ class App extends Component {
   // }
 
   render() {
-    return (
-      <div>
-        <Layout>
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" component={BurgerBuilder} exact />
+        <Redirect to="/" />
+      </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
           <Route path="/" component={BurgerBuilder} exact />
-        </Layout>
+          <Redirect to="/" />
+        </Switch>
+      )
+    }
+
+    return (
+      <div>
+        <Layout>{routes}</Layout>
       </div>
     )
   }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
