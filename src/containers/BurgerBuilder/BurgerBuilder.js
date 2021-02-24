9ebda11dccbdb7f33aca8f3536e0d4fa67631cc1
avishka964 <avishkakapuruge@gmail.com>
2021-02-24 08:21:45 +0530

    testing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,7 +10,7 @@ import axios from "../../axios-order"
 import withErrorHandler from "../../hoc/WithErrorHandler/WithErrorHandler"
 import * as actions from "../../store/actions/index"
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   // constructor(props) {
   //     super(props);
   //     this.state = {...}
